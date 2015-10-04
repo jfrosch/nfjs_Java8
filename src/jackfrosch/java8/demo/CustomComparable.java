7c875089f91cc7ed24ff22efe005feb474556892
jackfrosch <jfrosch@objistics.com>
2015-10-04 13:49:34 -0500

    Added comment

--- src/jackfrosch/java8/demo/CustomComparable.java
+++ src/jackfrosch/java8/demo/CustomComparable.java
@@ -2,6 +2,9 @@ package jackfrosch.java8.demo;
 
 public interface CustomComparable<T> extends Comparable<T> {
 
+    // This is contrived!!! From JavaDoc for Comparable:
+    //   "It is strongly recommended (though not required) that
+    //    natural orderings be consistent with equals."
     default boolean isComparativelyEqual(T o) {
         return compareTo(o) == 0;
     }
