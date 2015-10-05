55d7ee7808396bc7e47c954b32103291a3f8ad9a
jackfrosch <jfrosch@objistics.com>
2015-10-05 16:30:38 -0500

    Added functional example

--- src/jackfrosch/java8/demo/streams/ImperativeExample.java
+++ src/jackfrosch/java8/demo/streams/ImperativeExample.java
@@ -5,10 +5,15 @@ import java.math.BigDecimal;
 public class ImperativeExample {
 
     public static void main(String[] args) {
+
+        long start = System.currentTimeMillis();
+
         Order order = OrderFactory.createOrder();
         System.out.println("Non-taxable total: " + order.calculateNontaxableTotal());
         System.out.println("Taxable total: " + order.calculateTaxableTotal());
         System.out.println("Tax: " + order.calculateTax());
         System.out.println("Order total: " + order.calculateTotal());
+
+        System.out.println("Total time (secs): " + (System.currentTimeMillis() - start) / 1000.0);
     }
 }
