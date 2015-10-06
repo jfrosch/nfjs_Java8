b6da4cf50e02002213c39ec20b03f34e8b7884f5
jackfrosch <jfrosch@objistics.com>
2015-10-05 23:39:10 -0500

    initial commit

---
+++ src/jackfrosch/java8/demo/groovy/GroovyWithJava8.groovy
@@ -0,0 +1,20 @@
+import java.util.function.Predicate
+import java.util.stream.Collectors
+
+public List<Integer> pickSome(List<Integer> values,
+                              Predicate<Integer> pickingStrategy) {
+    values.stream()
+          .filter(pickingStrategy)
+          .collect(Collectors.toList())
+}
+
+List<Integer> values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+List<Integer> evens = pickSome(values, {value -> value % 2 == 0})
+println evens
+
+List<Integer> odds = pickSome(values) { value -> value % 2 != 0 }
+println odds
+
+def threes = values.findAll { value -> value % 3 == 0 }
+println threes
